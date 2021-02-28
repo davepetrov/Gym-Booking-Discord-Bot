a0c2b8149ee4498d58c1ec350139a450b29cf495
davepetrov <davidpetrovx@gmail.com>
2021-02-28 00:39:40 -0500

    Added config option, cleaned up code:

--- discord-bot.js
+++ discord-bot.js
@@ -1,138 +1,184 @@
 const {Client, MessageEmbed} = require('discord.js');
 const bot = new Client();
-var token;
-// var token = 'DCDV8yCDqnVx3zFI28ybzCWebdAAPe9V';
 const exec = require('child_process').exec
-
-
 let PREFIX = "!";
 
-def isFit4lessUser(username):
-// go throug all entrys in db
-            // if matches users discord id:
-            //     user in the db
-            //     book with all the config
-            // else:
-            //     user not in the db
-            //     return that config is not setup, use !config
+function sendConfigErrorMessage(message){
+    const configmsg = new MessageEmbed()
+        .setTitle("ERROR")
+        .setColor(0xff0000)
+        .setDescription('Dont forget to setup your configuration using !config');
 
+    message.author.send(configmsg) //private
+}
 
+function sendHelpMessage(message){
+    const helpmsg = new MessageEmbed()
+        .setTitle("HELP")
+        .setColor(0xff0000)
+        .setDescription(
+            'Setup saved configuration: !config [EMAIL]  [PASSWORD] [EXACT FIT4LESS LOCATION]  [MINIMUM TIME RANGE (24hr)]  [MAXIMUM  TIME RANGE (24hr)]\n'+
+            'Book a time manually: !book/n'+
+            'Check your booked times: !reserved\n'+
+            'Get all possible locations: !locations'+
+            'More help: !help');
 
-let db = new sqlite3.Database('./db/chinook.db', (err) => {
-    if (err) {
-      console.error(err.message);
+    message.author.send(helpmsg) //private
+}
+
+function sendLocationsMessage(message){
+    message.author.send("Available Locations", {
+        files:['./locations.txt']
+    });
+    message.author.send("Copy the location and use that as a location argument when using !book")
+}
+
+function sendHDefaultMessage(message){
+    const erromsg = new MessageEmbed()
+        .setTitle("ERROR")
+        .setColor(0xff0000)
+        .setDescription('Use help for correct usage(s)');
+
+    message.author.send(erromsg) //private
+}
+
+function isFit4lessUser(db, discordId){
+    // if matches users discord id:
+    //     user in the db
+    //     return true
+    // else:
+    //     user not in the db
+    //     return false
+
+    return db->query("SELECT EXISTS (SELECT * FROM USER ON discordId=${discordId})");
+
+    return exists;
+}
+
+function book(db, message){
+    var email =  db.exec("SELECT email FROM USER ON discordId=${userid}")
+    var password =  db.exec("SELECT password FROM USER ON discordId=${userid}")
+    var location =  db.exec("SELECT location FROM USER ON discordId=${userid}")
+    var begin =  db.exec("SELECT begin FROM USER ON discordId=${userid}")
+    var end =  db.exec("SELECT end FROM USER ON discordId=${userid}")
+    
+    sendBookingMessage(message){
+        const publicmsg = new MessageEmbed()
+        .setTitle('Booking set for user ${email} at ${location} from ${start} to ${end}')
+        .setColor(0xff0000)
+        .setDescription(("Checking Fit4less for available times, this may take a minute..."));
+
+    message.reply(publicmsg); //Public
+    exec('python3 fit4less-workout-booker.py book ${password} ${email} ${location} ${start} ${end}',
+        function (error, stdout, stderr) {
+            
+            const bookingmessage = new MessageEmbed()
+                .setTitle("You are booked for the following times")
+                .setColor(0xffa500)
+                .setDescription((stdout));
+
+            message.author.send(bookingmessage) //private
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
     }
-    console.log('Connected to the Fit4Less Booking database.');
-  });
+}
 
+checkReserved(db, message){
+    var email =  db.exec("SELECT email FROM USER ON discordId=${userid}")
+    var password =  db.exec("SELECT password FROM USER ON discordId=${userid}")
 
+    const publicmsg = new MessageEmbed()
+        .setTitle("Checking Fit4less for reserved times, this may take a minute...")
+        .setColor(0xff0000);
+    message.author.send(publicmsg);
+
+    exec('python3 fit4less-workout-booker.py reserved'+' '+password+' '+email,
+        function (error, stdout, stderr) {
+            const reservedmessage = new MessageEmbed()
+                .setTitle("You are booked for the following times")
+                .setColor(0xffa500)
+                .setDescription((stdout));
+            message.author.send(reservedmessage) //private
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+}
 
 bot.on('ready', () =>{
     console.log("Fit4Less Bot is now Online with new updates");
 })
 
 
-bot.on('message', message=>{
+bot.on('message', message=>{    
     if (!message.content.startsWith(PREFIX) || message.author.bot) return; // Not a command
 
+    let db = new sqlite3.Database('./db/fit.db', (err) => {
+        if (err) {
+        console.error(err.message);
+        }
+        console.log('Connected to the Fit4Less Booking database.');
+    });
+
+    var username = message.author.username;
+    var userid = message.author.id;
+
     let args = message.content.substring(PREFIX.length).split(" ");
-    var ready = 0;
     console.log(args.length, "arguments sent");
     
     switch (args[0]){
-        case 'config': //change config
-
-        // go throug all entrys in db
-        // if matches users discord id:
-        //     user in the db
-        //     change the config
-        // else:
-        //     user not in the db
-        //     create new entry with config
-
-        case 'book': //Books the time for you within the specfic time range
-
-            // go throug all entrys in db
-            // if matches users discord id:
-            //     user in the db
-            //     book with all the config
-            // else:
-            //     user not in the db
-            //     return that config is not setup, use !config
-
-            if (args.length==6){
-                console.log(location);
-                var password = args[1];
-                var email = args[2];
-                var location = args[3];
-                const publicmsg = new MessageEmbed()
-                    .setTitle("Booking set for user "+email+ " at " + location)
-                    .setColor(0xff0000)
-                    .setDescription(("Checking Fit4less for available times, this may take a minute..."));
-
-                message.reply(publicmsg); //Public
-                exec('python3 fit4less-workout-booker.py book'+' '+password+' '+email+ ' '+location+' ' + args[4] +' '+args[5],
-                    function (error, stdout, stderr) {
-                        
-                        const bookingmessage = new MessageEmbed()
-                            .setTitle("You are booked for the following times")
-                            .setColor(0xffa500)
-                            .setDescription((stdout));
-
-                        message.author.send(bookingmessage) //private
-                        if (error !== null) {
-                            console.log('exec error: ' + error);
-                        }
-                    });
-        
-            }else{
-                message.reply("Use !help for correct usage");
+        case 'config': 
+            if (args.length!=6){
+                sendHelpMessage(message);
+                break;
             }
+
+            var email =  args[1];
+            var password =  args[2];
+            var location =  args[3];
+            var begin =  args[4];
+            var end =  args[5];
+
+            if (!isFit4lessUser(db, userid)){
+                // add new entry
+                db.run('INSERT INTO USERS (discordId, email, password, location, begin, end) VALUES (${userid}, ${email}, ${password}, ${location}, ${begin}, ${end})');
+            }
+            // update the config
+            db.run('UPDATE USERS SET discordId=${userid}, email=${email}, password=${password}, location=${location}, begin=${begin}, end=${end}');
+            break;
+            
+        case 'book': //Books the time for you within the specfic time range
+            if (!isFit4lessUser(db, user)){
+                sendConfigErrorMessage(message);
+                break
+            }
+
+            //book with all the config
+            book(db, message);
             break;
 
         case 'reserved': //Lists you the times you are current booked for
-            if (args.length==3){
-                console.log(location);
-                var password = args[1];
-                var email = args[2];
-                const publicmsg = new MessageEmbed()
-                    .setTitle("Checking Fit4less for reserved times, this may take a minute...")
-                    .setColor(0xff0000);
-                message.author.send(publicmsg);
-                exec('python3 fit4less-workout-booker.py reserved'+' '+password+' '+email,
-                    function (error, stdout, stderr) {
-                        const reservedmessage = new MessageEmbed()
-                            .setTitle("You are booked for the following times")
-                            .setColor(0xffa500)
-                            .setDescription((stdout));
-                        message.author.send(reservedmessage) //private
-                        if (error !== null) {
-                            console.log('exec error: ' + error);
-                        }
-                    });
+            if (!isFit4lessUser(db, user)){
+                sendConfigErrorMessage(message);
+                break;
             }
-            else{
-                message.reply("Use !help for correct usage");
-            }
+
+            //check reserved times with all the config
+            checkReserved(db, message);
             break;
 
         case 'locations':
-            message.author.send("Available Locations", {
-                files:['./locations.txt']
-            });
-            message.author.send("Copy the location and use that as a location argument when using !book")
-            
+            sendLocationsMessage(message);
             break;
 
         case 'help':
-            message.reply("!book [PASSWORD]  [EMAIL]  [EXACT FIT4LESS LOCATION]  [MINIMUM TIME RANGE (24hr)]  [MAXIMUM  TIME RANGE (24hr)]");
-            message.reply("!reserved [PASSWORD]  [EMAIL]"); 
-            message.reply("!locations"); 
-            message.reply("!help");
+            sendHelpMessage(message);
             break;
             
         default:
-            message.author.send("Use !help for correct usage")
+            sendHDefaultMessage();
     }  
 })
 
