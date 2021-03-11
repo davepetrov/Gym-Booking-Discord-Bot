b19bf0995eb3f2ff6d647553e1669a9f16ec30c0
davepetrov <davidpetrovx@gmail.com>
2021-03-10 23:25:54 -0500

    Fixed more booking bugs in api, added autobooking (more optimized and checks continuously for new openings, fixed ui messages bug

---
+++ discord-bot.js
@@ -0,0 +1,377 @@
+const {Client, MessageEmbed} = require('discord.js');
+const bot = new Client();
+const execSync = require('child_process').execSync;
+const exec = require('child_process').exec;
+let PREFIX = "!";
+let db = require('better-sqlite3')('./db/fit.db');
+let dbName="USER"; // TEST for testing OR USER for deploy
+
+function setConfig(message, userid, email, password, location, begin, end){
+
+    if (!/^([01]\d|2[0-3]):?([0-5]\d)$/.test(begin) || !/^([01]\d|2[0-3]):?([0-5]\d)$/.test(end)){
+        sendFormatErrorMessage(message);
+        return;
+    }
+    console.log("setConfig");
+    
+    if (!isFit4lessUser(userid)){
+        console.log("not user, creating");
+        // add new entry
+        db.prepare(`INSERT INTO ${dbName} (discordId, email, password, location, begin, end) VALUES ('${userid}', '${email}', '${password}', '${location}', '${begin}', '${end}')`).run();
+        
+        const msg = new MessageEmbed()
+            .setTitle("CONFIG")
+            .setColor(0x61bf33)
+            .setDescription('Setting up new config for you. *You can now use !book to book*');
+        message.author.send(msg); 
+    }
+    else{
+        console.log("Is user, updating");
+        // update the config
+        db.prepare(`UPDATE ${dbName} SET email='${email}', password='${password}', location='${location}', begin='${begin}', end='${end}' WHERE discordId='${userid}'`).run();
+        
+        const msg = new MessageEmbed()
+            .setTitle(":muscle:CONFIG:muscle:")
+            .setColor(0x009cdf)
+            .setDescription('Updating your existing config. *You can now use !book to book*');
+        message.author.send(msg); 
+    }
+    return;
+}
+
+function sendConfigErrorMessage(message){
+    console.log("sendConfigErrorMessage");
+
+    const msg = new MessageEmbed()
+        .setTitle(":question:ERROR:question:")
+        .setColor(0xff0000)
+        .setDescription('Dont forget to setup your configuration using !config');
+
+    message.author.send(msg);
+    return;
+}
+
+function sendFieldErrorMessage(message, field){
+    console.log("sendFieldErrorMessage");
+
+    const msg = new MessageEmbed()
+        .setTitle(":question:ERROR:question:")
+        .setColor(0xff0000)
+        .setDescription(`Field ${field} is not a valid configuration, *please check !help* `);
+
+    message.author.send(msg);
+    return;
+}
+
+function sendFormatErrorMessage(message){
+    console.log("sendFieldErrorMessage");
+
+    const msg = new MessageEmbed()
+        .setTitle(":question:ERROR:question:")
+        .setColor(0xff0000)
+        .setDescription(`Your time slot time is in the wrong format. ##:## format is accepted`);
+
+    message.author.send(msg);
+    return;
+}
+
+function sendHelpMessage(message){
+    console.log("sendHelpMessage");
+
+    const msg = new MessageEmbed()
+        .setTitle(":question:HELP:question:")
+        .setColor(0xff0000)
+        .setDescription('Check out my github for a list of all available commands, with a full description of what each command does:\n https://github.com/davepetrov/Gym-Booking-Discord-Bot');
+
+    message.author.send(msg);
+    return;
+}
+
+function sendLocationsMessage(message){
+    console.log("sendLocationsMessage");
+
+    message.author.send("Available Locations", {
+        files:['./locations.txt']
+    });
+    message.author.send("Copy the location and use that as a location ar+gument when using !book")
+}
+
+function sendDefaultMessage(message){
+    console.log(":grey_question:sendDefaultMessage:grey_question:");
+    const msg = new MessageEmbed()
+        .setTitle("ERROR")
+        .setColor(0xff0000)
+        .setDescription('Use help for correct usage(s)');
+
+    message.author.send(msg);
+    return;
+}
+
+function isFit4lessUser(discordId){
+    const user = db.prepare(`SELECT * FROM ${dbName} WHERE discordId=${discordId}`).get();
+    return user!=undefined;
+}
+
+function book(message, discordId){
+    if (!isFit4lessUser(discordId)){
+        sendConfigErrorMessage(message);
+        return;
+    }
+
+    var user =  db.prepare(`SELECT * FROM ${dbName} WHERE discordId=${discordId}`).get()
+
+    console.log("Performing action on", user.email, user.password);
+    if (message!=null){
+        console.log("Booking time for user manually");
+
+        const msg = new MessageEmbed()
+        .setTitle(`:muscle:Booking:muscle:`)
+        .setColor(0xff0000)
+        .setDescription((`Checking Fit4less for available times, this may take a few seconds \nBooking set for  ${message.author.username} at ${user.location}`));
+
+        message.reply(msg); //Public
+    }
+    else{
+        console.log("Booking time for user automatically");
+    }
+
+    execSync(`python3 handler.py fit4less book ${user.password} ${user.email} ${user.location} ${user.begin} ${user.end}`,
+        function (error, stdout, stderr) {
+            console.log("Booking complete")
+    });
+
+    if (message!=null){
+        checkReserved(message,discordId);
+    }
+    
+}
+
+function autobook(discordId){
+    if (!isFit4lessUser(discordId)){
+        console.log("not user")
+        return;
+    }
+    var user =  db.prepare(`SELECT * FROM ${dbName} WHERE discordId=${discordId}`).get()
+
+    console.log("Performing action on", user.email, user.password);
+    console.log("Booking time for user automatically");
+    
+
+    exec(`python3 handler.py fit4less autobook ${user.password} ${user.email} ${user.location} ${user.begin} ${user.end}`,
+        function (error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error, stderr);
+            }
+            console.log("Booking complete")
+    });
+    
+}
+
+function checkReserved(message, discordId){
+    if (!isFit4lessUser(discordId)){
+        sendConfigErrorMessage(message);
+        return;
+    }
+
+    console.log("checkReserved");
+    var user =  db.prepare(`SELECT * FROM ${dbName} WHERE discordId=${discordId}`).get();
+
+    exec(`python3 handler.py fit4less reserved ${user.password} ${user.email}`,
+        function (error, stdout, stderr) {
+            if (message!=null){
+                const msg = new MessageEmbed()
+                    .setTitle(":grey_exclamation:Future bookings:grey_exclamation:")
+                    .setColor(0xffa500)
+                    .setDescription((stdout)+"\n Check your reserved times on the [Fit4less](https://myfit4less.gymmanager.com/portal/booking/index.asp?) site ");
+                message.author.send(msg) //private
+            }
+            if (error !== null) {
+                console.log('exec error: ' + error, stderr);
+            }
+            else{
+                console.log(stdout)
+                console.log("Checking reserved complete")
+            }
+        });
+}
+
+function autobookToggle(message, userid){
+    console.log("autobook toggle")
+
+    if (!isFit4lessUser(userid)){
+        sendConfigErrorMessage(message);
+        return;
+    }
+
+    var togglevalue =  db.prepare(`SELECT autobook FROM ${dbName} WHERE discordId=${userid}`).get().autobook;
+    var newtogglevalue = (togglevalue===0) ? 1 : 0
+    db.prepare(`UPDATE ${dbName} SET autobook=${newtogglevalue} WHERE discordId=${userid}`).run();
+
+    console.log(togglevalue, "->", newtogglevalue);
+    if (newtogglevalue===0){
+        const msg = new MessageEmbed()
+            .setTitle(":dash:AUTOBOOK:dash:")
+            .setColor(0xFF0000) 
+            .setDescription(`Autobook feature Toggled OFF - *You will have to manually use !book to book for the next 3 days*`);
+        message.author.send(msg) ;
+
+    }else{
+        const msg = new MessageEmbed()
+            .setTitle(":fire:AUTOBOOK:fire:")
+            .setColor(0x00FF00) 
+            .setDescription(`Autobook feature Toggled ON - *You will now be autobooked and guaranteed a spot until this feature is toggled off*`);
+        message.author.send(msg) ;
+    }
+    return;
+}
+
+function updateField(message, userid, fieldKey, fieldVal){
+
+    if (fieldKey=="begin" || fieldKey=="end"){
+        var time = fieldVal;
+        if (!/^([01]\d|2[0-3]):?([0-5]\d)$/.test(time)){
+            sendFormatErrorMessage(message);
+            return;
+        }
+
+    }
+    console.log(`updating field: ${fieldKey}: ${fieldVal}`);
+
+    if (!isFit4lessUser(userid)){
+        sendConfigErrorMessage(message);
+        return;
+    }
+
+    const msg = new MessageEmbed()
+        .setTitle("CONFIG")
+        .setColor(0x009cdf) 
+        .setDescription(`Updating your config **${fieldKey}** with **${fieldVal}**`);
+    message.author.send(msg) ;
+
+    db.prepare(`UPDATE ${dbName} SET ${fieldKey}='${fieldVal}' WHERE discordId=${userid}`).run();
+    return;
+}
+
+// Bot online msg
+bot.on('ready', () =>{
+    console.log("Fit4Less Bot is now Online with new updates");
+})
+
+// Bot recieves prompt
+bot.on('message', message=>{  
+    if (!message.content.startsWith(PREFIX)) return; // Not a command
+    console.log("\n")
+
+    var username = message.author.username;
+    console.log("username:"+username)
+    var userid = message.author.id;
+    console.log("userid:"+userid)
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+    
+    switch (args[0]){
+        case 'config': 
+            if (args.length == 3){
+                switch (args[1]){
+                    case "-email":
+                        updateField(message, userid, "email", args[2]);
+                        break;
+
+                    case "-password":
+                        updateField(message, userid, "password", args[2]);
+                        break;
+
+                    case "-location":
+                        updateField(message, userid, "location", args[2]);
+                        break;
+
+                    case "-begin":
+                        updateField(message, userid, "begin", args[2]);
+                        break;
+
+                    case "-end":
+                        updateField(message, userid, "end", args[2]);
+                        break;
+
+                    default:
+                        sendFieldErrorMessage(message, args[2]);
+                        break;
+                }
+                break;
+            }
+
+            if (args.length!=6){
+                sendHelpMessage(message);
+                break;
+            }
+            var email =  args[1];
+            var password =  args[2];
+            var location =  args[3];
+            var begin =  args[4];
+            var end =  args[5];
+            setConfig(message, userid, email, password, location, begin, end);
+            break;
+            
+        case 'autobook':
+            //Toggle Autobooking
+            autobookToggle(message, userid);
+            break;
+
+        case 'book': //Books the time for you within the specfic time range
+            //book with all the config
+            book(message, userid);
+            break;
+
+        case 'reserved': //Lists you the times you are current booked for
+            //check reserved times with all the config
+            checkReserved(message, userid);
+            break;
+
+        case 'locations':
+            sendLocationsMessage(message);
+            break;
+
+        case 'help':
+            sendHelpMessage(message);
+            break;
+            
+        default:
+            sendDefaultMessage(message);
+    }  
+})
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+    if (!channel) return;
+
+    // Send the message, mentioning the member
+    channel.send(`Welcome to Fit4Less Bot Server, ${member}`);
+});
+
+// Autobook for all the users with autobooking toggled on
+setInterval(function(){
+    console.log("[Checking  autobook...]")
+    var date = new Date(); // Create a Date object to find out what time it is
+    var datezone = date.getTime() + (date.getTimezoneOffset() * 60000);
+    var estDate = new Date(datezone - (3600000*5));
+
+    //Book at 12:00am EST
+    // if(estDate.getHours() === 0 && estDate.getMinutes() === 1){ 
+        var toggledUsers = db.prepare(`Select * from ${dbName} WHERE ${dbName}.autobook=1`).all();
+        toggledUsers.forEach(function (user) {
+            console.log(`\nAutobooking for ${user.discordId}`);
+            autobook(user.discordId)
+        });
+   // }
+}, 60000); // Repeat every 60000 milliseconds (1 min)
+
+//Run
+const fs = require('fs');
+const data = fs.readFileSync('discord-hidden-key.txt', 'UTF-8');
+const lines = data.split(/\r?\n/);
+lines.forEach((line) => {
+    var token = line;
+    bot.login(token);
+});
---
+++ handler.py
@@ -0,0 +1,381 @@
+# Usage: python3 handler.py [gym] [command] [pass] [email] [location] [minimum time to book] [maximum time to book]
+# Prereq: Times are in military format (##:##)
+#        Commands include: book, reserved, locations, autobook
+#        Gyms include 'fit4less', 'lafitness'
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException        
+import os
+import sys
+from time import sleep
+import datetime
+
+
+def scrollTo(driver, element):
+    driver.execute_script("""arguments[0].scrollIntoView({
+            block: 'center',
+            inline: 'center'
+        });""", element)
+    driver.execute_script("arguments[0].scrollIntoView();", element);
+    return element
+
+def elementByXpathExists(driver, xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+def elementByIDExists(driver, id):
+    try:
+        driver.find_element_by_id(id)
+    except NoSuchElementException:
+        return False
+    return True
+
+def elementByCssSelectorExists(driver, selector):
+    try:
+        driver.find_element_by_css_selector(selector)
+    except NoSuchElementException:
+        return False
+    return True
+
+class Fit4lessAccount():
+    '''
+    Account associated with fit4less account
+    '''
+
+    def __init__(self, password, emailaddress):
+        self.password = password
+        self.email = emailaddress
+        self.countbooked = 0
+        self.timesbooked = {}
+
+    def getPassword(self):
+        return self.password
+
+    def getEmailAddress(self):
+        return self.email
+
+    def login(self, driver):
+        try:
+            driver.get('https://myfit4less.gymmanager.com/portal/login.asp')
+
+            # Find username/email box, set
+            email = scrollTo(driver, driver.find_element_by_id('emailaddress'))
+            email.send_keys(self.getEmailAddress())
+
+            # Find password box, set
+            password = scrollTo(driver, driver.find_element_by_id('password'))
+            password.send_keys(self.getPassword())
+
+            # Find login button, click
+            login_button = scrollTo(driver, driver.find_element_by_id('loginButton'))
+            login_button.click()
+
+            if elementByXpathExists(driver, '/html/body/div[2]/div/div/div/div/h1'):
+                if driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div/h1').text == 'LOG IN FAILED':
+                    print("Incorrect credentials, check again")
+                    return False
+                return True
+            return True
+        except Exception as e:
+            print("Something went wrong with loggin in:" + str(e))
+
+    def isMaxedBook(self, driver): 
+        try:
+            if not elementByXpathExists(driver, '//*[@id="doorPolicyForm"]/h2'):
+                return False
+
+            if scrollTo(driver, driver.find_element_by_xpath('//*[@id="doorPolicyForm"]/h2')).text=='Maximum personal reservations reached':
+                return True
+            
+        except Exception as e:
+            print("BookingErr:", str(e))
+
+        return False
+
+    def isClosed(self, driver):
+        try:
+            if not self.login(driver):
+                return True
+
+            if elementByXpathExists(driver, "/html/body/div[2]/div/div/div/div/h1"):
+                title = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/h1").text
+                if title == "Your club is closed":
+                    print("Your gym is closed")
+                    return True
+            return False
+
+        except Exception as e:
+            return False
+
+    def bookTime(self, driver):
+        alltimes_elements = driver.find_elements_by_css_selector(".available-slots > .time-slot")
+
+        if len(alltimes_elements) == 0:
+            return 0
+
+        for time in alltimes_elements:
+            clock = time.get_attribute("data-slottime")[3::]
+            time_id = time.get_attribute("id")
+            index_of_colon = clock.find(':')
+            index_of_space = clock.find(' ')
+            hour, minute = 0, 0
+            hour += int(clock[:index_of_colon])
+            minute = int(clock[index_of_colon+1:index_of_space])
+            if clock[-2:] == "PM":
+                if hour == 12:
+                    pass
+                else:
+                    hour += 12
+            elif clock[-2:] == "AM" and hour == 12:
+                hour = 0
+
+            timegym = datetime.datetime.now().replace(hour=int(hour), minute=int(minute), second=0, microsecond=0)
+
+            if minrangetimegym <= timegym <= maxrangetimegym:
+
+                # Book this time
+                sleep(1)
+                booktime = scrollTo(driver, driver.find_element_by_id(time_id))
+                booktime.click()  # Click on the specifc time to book, falling in the time domain we want
+
+                # Accept COVID-19 terms of service
+                sleep(1)
+                driver.find_element_by_id("dialog_book_yes").click()
+                return clock
+
+        print()
+        return 0
+
+    def autobook(self, driver, location, minrangetimegym, maxrangetimegym):
+        # 1) Enter https://www.fit4less.ca/ > 2) Book workout
+        try:
+
+            if self.isMaxedBook(driver):
+                print("Maximum # of slots booked 1")
+                return
+
+            if not elementByIDExists(driver, 'btn_club_select'):
+                print("Maximum # of slots booked")
+                return
+
+            selectclub_element = scrollTo(driver, driver.find_element_by_id('btn_club_select'))
+            selectclub_element.click()
+            
+
+            if not elementByXpathExists(driver, "//div[contains(text(),'{}')]".format(location)):
+                print("Incorrect location, try again")
+                return
+
+            location_element = driver.find_element_by_xpath("//div[contains(text(),'{}')]".format(location))
+            location_element.click()
+
+
+            # 5) Select Day: Ex: Tomorrow. Check todays date, select tomorrows date (Maximum of 3 days in advance)
+            today = datetime.date.today()
+            dayaftertomorrow = (today + datetime.timedelta(days=2)).strftime("%Y-%m-%d")
+
+            sleep(3)
+            print("last day checking", dayaftertomorrow)
+
+            if not elementByIDExists(driver, 'btn_date_select'):
+                print("Maximum booking for this user")
+                return
+
+            selectday_element = scrollTo(driver, driver.find_element_by_id('btn_date_select'))
+            selectday_element.click()
+            day_element_name = "date_"+dayaftertomorrow
+            driver.find_element_by_id(day_element_name).click()
+
+            booked = self.bookTime(driver)
+            if booked != 0:
+                self.timesbooked[dayaftertomorrow] = booked
+            
+        except Exception as e:
+            print("BookingErr:"+ str(e))
+
+        print(" ")
+        return 1
+
+    def book(self, driver, location, minrangetimegym, maxrangetimegym):
+        # 1) Enter https://www.fit4less.ca/ > 2) Book workout
+        try:
+            if self.isMaxedBook(driver):
+                print("Maximum # of slots booked 1")
+                return
+                
+            if not elementByIDExists(driver, 'btn_club_select'):
+                print("Maximum # of slots booked")
+                return
+
+            selectclub_element = scrollTo(driver, driver.find_element_by_id('btn_club_select'))
+            selectclub_element.click()
+            
+
+            if not elementByXpathExists(driver, "//div[contains(text(),'{}')]".format(location)):
+                print("Incorrect location, try again")
+                return
+
+            location_element = driver.find_element_by_xpath("//div[contains(text(),'{}')]".format(location))
+            location_element.click()
+
+
+            # 5) Select Day: Ex: Tomorrow. Check todays date, select tomorrows date (Maximum of 3 days in advance)
+            today = datetime.date.today()
+            tomorrow = today + datetime.timedelta(days=1)
+            dayaftertomorrow = today + datetime.timedelta(days=2)
+            days = [dayaftertomorrow.strftime("%Y-%m-%d"), tomorrow.strftime("%Y-%m-%d"), today.strftime("%Y-%m-%d")]  # Book 3 days in advance
+
+            for i in days:
+                sleep(3)
+                print("checking", i)
+                # try:
+                #     countbooked = driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div/form/p[3]")
+                # except:
+                #     print("No other timeslots available, Booked {} times".format(self.countbooked))
+                #     return
+
+                # self.countbooked = countbooked.text[9]
+                continueXpath='/html/body/div[2]/div/div/div/div/div/div'
+                if elementByXpathExists(driver, continueXpath):
+                    print("Daily limit reached")
+                    driver.find_element_by_xpath(continueXpath).click()
+                    sleep(1)
+
+                if not elementByIDExists(driver, 'btn_date_select'):
+                    print("Maximum booking for this user")
+                    return
+
+                selectday_element = scrollTo(driver, driver.find_element_by_id('btn_date_select'))
+                selectday_element.click()
+                day_element_name = "date_"+i
+                driver.find_element_by_id(day_element_name).click()
+
+                booked = self.bookTime(driver)
+                if booked != 0:
+                    self.timesbooked[i] = booked
+                    print('booked')
+            
+        except Exception as e:
+            print("BookingErr:", str(e))
+
+        print(" ")
+        return 1
+
+    def getReserved(self, driver):
+    
+        try:
+            alltimes_elements = driver.find_elements_by_css_selector(".reserved-slots > .time-slot")
+            #reservedSlots=[]
+            for i in alltimes_elements:
+                # Very hack-ish, fix in future
+                print('-', i.get_attribute('data-slotdate'), i.get_attribute('data-slotclub'), i.get_attribute('data-slottime'))
+                #reservedSlots.add((i.get_attribute('data-slotdate'), i.get_attribute('data-slotclub'), i.get_attribute('data-slottime')))
+            
+            if len(alltimes_elements)==0:
+                print("No bookings, try picking a different location or changing your available time interval")
+    
+        except Exception as e:
+            print("ReserveErr:", str(e))
+    
+        #return reservedSlots
+
+class LAFitnessAccount():
+    '''
+    Account associated with LA Fitness account
+    '''
+
+    def __init__(self, password, emailaddress):
+        self.password = password
+        self.email = emailaddress
+        self.countbooked = 0
+        self.timesbooked = {}
+
+    def getPassword(self):
+        return self.password
+
+    def getEmailAddress(self):
+        return self.email
+
+    def login(self, driver):
+        driver.get('https://www.lafitness.com/Pages/login.aspx')
+
+        # Find username/email box, set
+        email = scrollTo(driver, driver.find_element_by_xpath('//*[@title="Username"]'))
+        email.send_keys(self.getEmailAddress())
+
+        # Find password box, set
+        password = scrollTo(driver, driver.find_element_by_xpath('//*[@title="Password"]'))
+        password.send_keys(self.getPassword())
+
+        # Find login button, click
+        login_button = scrollTo(driver, driver.find_element_by_xpath('//*[@value="Sign in"'))
+        login_button.click()
+
+
+    def bookTime(self, driver):
+        pass
+
+    def getReserved(self, driver):
+        pass
+        
+    def isClosed(self, driver):
+        pass
+
+
+
+if __name__ == '__main__':
+
+    gym = sys.argv[1]
+    function = sys.argv[2]  # command
+    password = sys.argv[3]
+    email = sys.argv[4]
+    if (gym=='fit4less'): person = Fit4lessAccount(password, email)
+    elif (gym=='lafitness'): person = LAFitnessAccount(password, email)
+    else: print("Unknown Gym"); sys.exit();
+
+    options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # options.add_argument('window-size=1920x1080');
+    # driver = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver'), options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+
+    if person.isClosed(driver):
+        driver.quit()
+        sys.exit();
+
+    if function == 'book':
+        location = sys.argv[5].replace('-', ' ')
+        start_time = sys.argv[6]
+        end_time = sys.argv[7]
+        minrangetimegym = datetime.datetime.now().replace(hour=int(start_time[:start_time.find(":")],), minute=int(start_time[start_time.find(":")+1:]))
+        maxrangetimegym = datetime.datetime.now().replace(hour=int(end_time[:end_time.find(":")]), minute=int(end_time[end_time.find(":")+1:]))
+        if person.login(driver):
+            person.book(driver, location, minrangetimegym, maxrangetimegym)
+
+    elif function == 'autobook':
+        location = sys.argv[5].replace('-', ' ')
+        start_time = sys.argv[6]
+        end_time = sys.argv[7]
+        minrangetimegym = datetime.datetime.now().replace(hour=int(start_time[:start_time.find(":")],), minute=int(start_time[start_time.find(":")+1:]))
+        maxrangetimegym = datetime.datetime.now().replace(hour=int(end_time[:end_time.find(":")]), minute=int(end_time[end_time.find(":")+1:]))
+        
+        if person.login(driver):
+            person.autobook(driver, location, minrangetimegym, maxrangetimegym)
+
+    elif function == 'reserved':
+        if person.login(driver):
+            person.getReserved(driver)
+
+    else:
+        print("Unknown command")
+
+    driver.quit()
+    sys.exit();
+
